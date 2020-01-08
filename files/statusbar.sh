f1cf26f750a72908651820c650f1eaeb20a53086
Geir André Lund <geir.andre.lund@nav.no>
2020-01-08 11:29:17 +0100

    Force screen resolution (new monitors)

--- files/statusbar.sh
+++ files/statusbar.sh
@@ -3,8 +3,9 @@
 while true; do
     hostname=`hostname`
     uptime=`uptime`
+    resolution=`xdpyinfo | awk '/dimensions/{print $2}'`
     ip=`ip addr show dev wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1`
     avail=`free -h | grep 'Mem' | awk '{print $7}'`
-    echo "$uptime | $hostname | WLAN $ip | $avail free"
+    echo "$resolution | $uptime | $hostname | WLAN $ip | $avail free"
     sleep 1
 done
